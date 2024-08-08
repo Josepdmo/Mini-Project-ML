b44722719a9484d9a5df8b24f3f4b24465d64edb
Josepdmo <pepdemarti@gmail.com>
2024-08-08 16:47:32 +0200

    update

--- functions.py
+++ functions.py
@@ -21,3 +21,13 @@ position_mapping = {
 # Function to replace position names with abbreviations
 def abbreviate_positions(position):
     return position_mapping.get(position, position)
+
+def remove_outliers(df, columns):
+    for column in columns:
+        Q1 = df[column].quantile(0.25)
+        Q3 = df[column].quantile(0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - 1.5 * IQR
+        upper_bound = Q3 + 1.5 * IQR
+        df = df[(df[column] >= lower_bound) & (df[column] <= upper_bound)]
+    return df
